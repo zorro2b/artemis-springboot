6f80ac1137786bf7a0621dfebf6297d3c576997a
Andrew Zahra <azahra@itree.com.au>
2020-11-09 13:38:02 +1100

    Calc latency

--- src/main/java/com/example/amqdemo/Receiver.java
+++ src/main/java/com/example/amqdemo/Receiver.java
@@ -1,14 +1,22 @@
 package com.example.amqdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 public class Receiver {
+  private Logger logger = LoggerFactory.getLogger(this.getClass());
 
   @JmsListener(destination = "topic1", containerFactory = "myFactory")
   public void receiveMessage(CustomMessage msg) {
-    System.out.println("Received <" + msg + ">");
+    int transfer = (LocalDateTime.now().getNano() - msg.getSent().getNano())/1000;
+    logger.info("Message read from topic 1 : " + msg
+            + " transfer time: "
+            + (transfer) + "µs");
   }
 
 }
