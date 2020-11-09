4faff80890448d3f9f58e8cdb108e1f05829949b
Andrew Zahra <azahra@itree.com.au>
2020-11-09 11:11:34 +1100

    Initial

---
+++ src/main/java/com/example/amqdemo/AmqdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.amqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmqdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmqdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/amqdemo/AmqdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
